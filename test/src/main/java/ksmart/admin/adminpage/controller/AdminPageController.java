bbeb46e710e0f1a6c25d95c353aa47da19c5166f
exb0y <96f22l@gmail.com>
2025-06-27 15:43:40 +0900

    [lookon-jsh] 상품등록 detail : 상세 변경 내용

--- test/src/main/java/ksmart/admin/adminpage/controller/AdminPageController.java
+++ test/src/main/java/ksmart/admin/adminpage/controller/AdminPageController.java
@@ -3,10 +3,8 @@ package ksmart.admin.adminpage.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class AdminPageController {
-	/* fuckyou infp_gy */
 	@GetMapping("/")
 	public String Mainview() {
 		
-		return "/main";
 	}
 }
